5524a65337e2263ef1a2b88370f2dfb0bf0ab480
Joona Keskitalo <joona.keskitalo@gmail.com>
2023-10-29 15:50:01 +0200

    Add initial express setup

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,45 @@ const { AceBase } = require("acebase");
 
 morgan("tiny");
 
-const db = new AceBase("my_db");
-
 const app = express();
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+
+// const db = new AceBase("my_db");
+
+app.get("", (req, res) => {
+  return res.send("Burgers API");
+});
+
+app.get("/burgers", (req, res) => {
+  const mockResponse = [
+    { id: "1", name: "Example burger" },
+    { id: "2", name: "Example burger" },
+    { id: "3", name: "Example burger" },
+    { id: "4", name: "Example burger" },
+    { id: "5", name: "Example burger" },
+  ];
+
+  return res.json(mockResponse);
+});
+
+const notFoundError = (req, res, next) => {
+  res.status(404);
+  const error = new Error("Not Found");
+  next(error);
+};
+
+const errorHandler = (error, req, res) => {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: error.message,
+  });
+};
+
+app.use(notFoundError);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
