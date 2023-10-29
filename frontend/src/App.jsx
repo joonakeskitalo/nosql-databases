2efc004eb7e433015e88b947906400304bf7ac7e
Joona Keskitalo <joona.keskitalo@gmail.com>
2023-10-29 16:22:02 +0200

    Add data fetching, list items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const Card = ({ id, name }) => {
+  return (
+    <div className="card">
+      {name} ({id})
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const baseUrl = "http://localhost:3000";
+  const burgersUri = `${baseUrl}/burgers`;
+  const [burgers, setBurgers] = useState(null);
+
+  const fetchData = async () => {
+    const req = await axios.get(burgersUri);
+    const data = req.data;
+    setBurgers(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(`🔥 burgers`, burgers);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Burgers Feed</h1>
+      {burgers?.map((x) => (
+        <Card key={x.id} name={x.name} id={x.id} />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
